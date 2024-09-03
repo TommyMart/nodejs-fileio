df963fa75838ba0587149eda21b42308d0ee37ba
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-09-03 19:32:05 +0930

    feat: async and sync file fs writer

--- src/index.js
+++ src/index.js
@@ -7,3 +7,37 @@ let defaultEnv = {
 }
 
 console.log(defaultEnv)
+
+let contentToWrite = "";
+
+// PORT = 3030
+// SOME_DATABASE_URL = blah blah
+
+// ["PORT", "SOME_DATABASE_URL", ....]
+Object.keys(defaultEnv).forEach(envKey => {
+    // Add a line to contentToWrite with they key name and key value
+    contentToWrite += `${envKey}=${defaultEnv[envKey]}\n`;
+});
+
+console.log(contentToWrite);
+
+
+const fs = require("node:fs");
+
+// fs.writeFileSync(".env", contentToWrite);
+// console.log("File has been written");
+
+console.log("Before fs callback");
+
+// Asyncronous way of writing files with error handling
+// fs.writeFile(filePath, fileContents, callback);
+fs.writeFile(".env", contentToWrite, (error) => {
+    if (error){
+        console.log("File writing had errors!");
+    } else {
+        console.log("File has been written!");
+        
+    }
+});
+
+console.log("After fs callback");
