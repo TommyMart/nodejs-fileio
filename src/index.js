8d2f66d5ca90db4f436d9bf5089ebd07f2bfeb47
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-09-03 19:00:17 +0930

    feat: project init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let defaultEnv = {
+    PORT: 3030,
+    SOME_DATABASE_URL: "blah blah",
+    SECURITY_TOKEN: "jfseifjesi",
+    SECRET_API_KEY: "rroewjwojf",
+    JWT_SECRET_KEY: "fodfksfk osfskffsfoskffkfs"
+}
+
+console.log(defaultEnv)
