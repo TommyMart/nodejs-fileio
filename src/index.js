eff9793b43e4ef51b028d68e0818244b8bf04f12
Thomas Martin <161907136+TommyMart@users.noreply.github.com>
2024-09-03 20:16:14 +0930

    feat: async await example

--- src/index.js
+++ src/index.js
@@ -21,23 +21,61 @@ Object.keys(defaultEnv).forEach(envKey => {
 
 console.log(contentToWrite);
 
+// Promises based version of node fs
+const fs = require("node:fs/promises");
 
-const fs = require("node:fs");
 
-// fs.writeFileSync(".env", contentToWrite);
-// console.log("File has been written");
-
-console.log("Before fs callback");
-
-// Asyncronous way of writing files with error handling
-// fs.writeFile(filePath, fileContents, callback);
-fs.writeFile(".env", contentToWrite, (error) => {
-    if (error){
-        console.log("File writing had errors!");
-    } else {
-        console.log("File has been written!");
-        
+async function writeEnvFile(){
+    console.log("before await")
+    try {
+        let result = await fs.writeFile(".env", contentToWrite);
+        console.log(result)
+    } catch (error) {
+        console.log("Error occured writing the file: ", error);
     }
-});
+    
+    console.log("After await")
+}
 
-console.log("After fs callback");
+writeEnvFile();
+
+
+
+
+// console.log("before promise")
+// fs.writeFile(".env", contentToWrite).then(() => {
+//     console.log("After the file has been written");
+// }).then(() => {
+
+    
+//     console.log("After the file has been written (second time)");
+// }).catch((error) => {
+//     console.log("This error occured " + error);
+// })
+// console.log("After promise")
+
+
+
+
+
+
+// File System
+// const fs = require("node:fs");
+
+// // fs.writeFileSync(".env", contentToWrite);
+// // console.log("File has been written");
+
+// console.log("Before fs callback");
+
+// // Asyncronous way of writing files with error handling
+// // fs.writeFile(filePath, fileContents, callback);
+// fs.writeFile(".env", contentToWrite, (error) => {
+//     if (error){
+//         console.log("File writing had errors!");
+//     } else {
+//         console.log("File has been written!");
+        
+//     }
+// });
+
+// console.log("After fs callback");
